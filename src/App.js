57226d0a48896d64d16bb9f5fae36b07d5fab029
ElucG <2879501726@qq.com>
2023-08-15 22:54:26 +0800

    v1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import san from './img/san.jpg'
+import ysz from './img/ysz.jpg'
+import love from './img/love.gif'
 
-function App() {
+export default function App() {
+  const [hitokoto, setHitokoto] = useState('')
+
+  useEffect(() => {
+    axios({
+      url: 'https://v1.hitokoto.cn',
+      method: 'get',
+      params: {
+        c: 'a'
+      }
+    }).then(res =>
+      setHitokoto(`${res.data.hitokoto} \n\t\t\t\t\t\t\t--《${res.data.from}》`)
+    )
+  }, [])
+
+  const handleClick = () => {
+    axios({
+      url: 'https://v1.hitokoto.cn',
+      method: 'get',
+      params: {
+        c: 'a'
+      }
+    }).then(res =>
+      setHitokoto(`${res.data.hitokoto} \n\t\t\t\t\t\t\t--《${res.data.from}》`)
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ whiteSpace: 'pre', marginTop: 100, display: 'flex', justifyContent: 'center' , fontSize:40}}>
+        {hitokoto}
+      </div>
+      <div style={{ marginTop: 100, display: 'flex', justifyContent: 'center' }}>
+        <img src={ysz} style={{ width: 400, height: 400 }} />
+        <img src={love} style={{ width: 400, height: 400 }} onClick={handleClick}/>
+        <img src={san} style={{ width: 400, height: 400 }} />
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
